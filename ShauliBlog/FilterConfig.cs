886b77db2aec658adf20a07f3cfba12cac0c0947
Matan Ashkenazi <matanashkenazi@gmail.com>
2016-10-16 14:40:22 +0300

    test1 MATAN

---
+++ ShauliBlog/FilterConfig.cs
@@ -0,0 +1,11 @@
+using System.Web.Mvc;
+
+namespace ShauliBlog{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
+        }
+    }
+}
